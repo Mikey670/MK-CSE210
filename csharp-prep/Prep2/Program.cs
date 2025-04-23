237d3178af8c04e394f2d9b82e59916a5d817245
Mikey670 <71160568+Mikey670@users.noreply.github.com>
2025-04-23 14:15:18 -0700

    Finished C# prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,49 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What % Grade did you get? ");
+        int grade = int.Parse(Console.ReadLine());
+
+        //Find Base letter grade based off grade range
+        string letterGrade;
+        if (grade >= 90)
+        {
+            letterGrade = "A";
+        }
+        else if (grade >= 80)
+        {
+            letterGrade = "B";
+        }
+        else if (grade >= 70)
+        {
+            letterGrade = "C";
+        }
+        else if (grade >= 60)
+        {
+            letterGrade = "D";
+        }
+        else
+        {
+            letterGrade = "F";
+        }
+
+        //Decide wether to use "a" or "an" before the letter grade announcement
+        string a = "a";
+        if (letterGrade == "A" || letterGrade == "F") { a = "an"; }
+
+        //Use Modulo operator to find the value of the right-most digit
+        int rightmostDigit = grade % 10;
+
+        //Decide wether to add "+" or "-" to grade
+        if (rightmostDigit >= 7 && letterGrade != "A" && letterGrade != "F")
+        {
+            letterGrade += "+";
+        }
+        else if (rightmostDigit < 3 && letterGrade != "F" && grade < 100)
+        {
+            letterGrade += "-";
+        }
+
+        Console.WriteLine($"Your {grade}% score is {a} {letterGrade}");
     }
 }
